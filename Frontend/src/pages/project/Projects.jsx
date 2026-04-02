fc511368763b27c984dc6d69e8beaf6b53669e6b
Raj kumar singh <162801401+raj8863@users.noreply.github.com>
2026-04-02 23:15:40 +0530

    Update API endpoint for fetching projects

--- Frontend/src/pages/project/Projects.jsx
+++ Frontend/src/pages/project/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = ({ showHero = true }) => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/projects");
+        const response = await fetch("https://marketing-b3je.onrender.com/api/projects");
         const data = await response.json();
         setProjectsData(data);
       } catch (error) {
